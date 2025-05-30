2742926115afa47b5a0ce21b9bd123193b87ee6e
Vsevolod Strukchinsky <floatdrop@gmail.com>
2025-05-30 22:49:25 +0500

    Add orders.go stub

---
+++ sdk/orders.go
@@ -0,0 +1,12 @@
+package investgo
+
+import (
+	"context"
+
+	pb "github.com/strukchinsky/tbank-invest-go-sdk"
+)
+
+func (c *Client) PostOrder(ctx context.Context, request *pb.PostOrderRequest) (*pb.PostOrderResponse, error) {
+	client := pb.NewOrdersServiceClient(c.conn)
+	return client.PostOrder(ctx, request)
+}
