ea2fe80f2b1e6d3dc3abc0af7194786766c2ff0b
Vsevolod Strukchinsky <floatdrop@gmail.com>
2025-05-30 15:51:50 +0500

    Modernize logger.go

--- sdk/logger.go
+++ sdk/logger.go
@@ -2,16 +2,16 @@ package investgo
 
 // Logger interface for structural logging
 type Logger interface {
-	Debug(msg string, keyvals ...interface{})
-	Info(msg string, keyvals ...interface{})
-	Warn(msg string, keyvals ...interface{})
-	Error(msg string, keyvals ...interface{})
+	Debug(msg string, keyvals ...any)
+	Info(msg string, keyvals ...any)
+	Warn(msg string, keyvals ...any)
+	Error(msg string, keyvals ...any)
 }
 
 // NoopLogger implements Logger interface but does nothing
 type NoopLogger struct{}
 
-func (NoopLogger) Debug(msg string, keyvals ...interface{}) {}
-func (NoopLogger) Info(msg string, keyvals ...interface{})  {}
-func (NoopLogger) Warn(msg string, keyvals ...interface{})  {}
-func (NoopLogger) Error(msg string, keyvals ...interface{}) {}
+func (NoopLogger) Debug(msg string, keyvals ...any) {}
+func (NoopLogger) Info(msg string, keyvals ...any)  {}
+func (NoopLogger) Warn(msg string, keyvals ...any)  {}
+func (NoopLogger) Error(msg string, keyvals ...any) {}
